e954fa62e7f8b9d2b747edbced170c7c842af58e
ilgazse <ilgaz.serim@argela.com.tr>
2019-06-24 17:08:30 +0300

    first commit

--- jenkins-files/spon-507.groovy
+++ jenkins-files/spon-507.groovy
@@ -10,23 +10,6 @@ node ("${podName}") {
                 git clone  https://@github.com/borougbuga/robot-spon.git.git"
             '''
         }
-        stage ('pip & robot framework installation') {
-            steps {
-                sh '''
-                    sudo apt install python-pip
-                    sudo apt install robotframework
-                '''
-            }
-        }
-        stage ('required libraries for robot-tests') {
-            steps {
-                sh '''
-                    sudo pip install --upgrade robotframework-sshlibrary
-                    sudo pip install -U requests
-                    sudo pip install -U robotframework-requests
-                '''
-            }
-        }
     }
 }
 
