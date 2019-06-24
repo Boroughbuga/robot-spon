a60af9c1692dc41781a332c43fc680833e35b6c7
ilgazse <ilgaz.serim@argela.com.tr>
2019-06-24 16:50:59 +0300

    first commit

--- jenkins-files/spon-507.groovy
+++ jenkins-files/spon-507.groovy
@@ -1,44 +1,37 @@
-pipeline {
-    node ("${podName}") {
-        agent any
-        stages {
-            stage('Sanity check') {
-                steps {
-                    input "Ready to start?"
-                }
+node ("${podName}") {
+    agent any
+    stages {
+        stage('Sanity check') {
+            steps {
+                input "Ready to start?"
             }
-            stage ('cloning form github') {
-                steps {
-                    sh '''
-                        sudo apt install git
-                        cd /home/cord/ilgaz
-                        git clone  https://@github.com/borougbuga/robot-spon.git.git"
-                    '''
-                }
+        }
+        stage ('cloning form github') {
+            steps {
+                sh '''
+                    sudo apt install git
+                    cd /home/cord/ilgaz
+                    git clone  https://@github.com/borougbuga/robot-spon.git.git"
+                '''
             }
-            stage ('pip & robot framework installation') {
-                steps {
-                    sh '''
-                        sudo apt install python-pip
-                        sudo apt install robotframework
-                    '''
-                }
+        }
+        stage ('pip & robot framework installation') {
+            steps {
+                sh '''
+                    sudo apt install python-pip
+                    sudo apt install robotframework
+                '''
             }
-            stage ('required libraries for robot-tests') {
-                steps {
-                    sh '''
-                        sudo pip install --upgrade robotframework-sshlibrary
-                        sudo pip install -U requests
-                        sudo pip install -U robotframework-requests
-                    '''
-                }
+        }
+        stage ('required libraries for robot-tests') {
+            steps {
+                sh '''
+                    sudo pip install --upgrade robotframework-sshlibrary
+                    sudo pip install -U requests
+                    sudo pip install -U robotframework-requests
+                '''
             }
         }
     }
-success {
-    echo 'pipeline completed'
 }
-failure {
-    echo 'pipeline failed'
-}
-}
+
