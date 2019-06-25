73b8139fae261fd2c25924bbec84d38dcb32e8cc
ilgazse <ilgaz.serim@argela.com.tr>
2019-06-25 16:26:47 +0300

    first commit

--- jenkins-files/spon-507-v2.groovy
+++ jenkins-files/spon-507-v2.groovy
@@ -1,40 +1,44 @@
 pipeline {
     parameters {
         string(
-                name: 'whichNode',
-                defaultValue: "192.168.31.181",
-                description: 'where do you want to run pipeline?',
-         )
+            name: 'whichNode',
+            defaultValue: "192.168.31.181",
+            description: 'where do you want to run pipeline?' )
         choice(
-                name: 'test1',
-                choices: "yes\nno",
-                description: 'test1:check-nodes.Choose yes to run the test' )
+            name: 'test1',
+            choices: "yes\nno",
+            description: 'test1:check-nodes.Choose yes to run the test' )
     }
     agent {
         node 'whichNode'
         }
     stages {
         stage ('cloning form github') {
-          sh'''
-            sudo apt install git
-            cd /home/cord/ilgaz
-            rm -rf robot-spon
-            git clone  "https://github.com/borougbuga/robot-spon.git"
-          '''
+            steps {
+                sh'''
+                sudo apt install git
+                cd /home/cord/ilgaz
+                rm -rf robot-spon
+                git clone  "https://github.com/borougbuga/robot-spon.git"
+                '''
+            }
         }
         stage ('pip & robot framework installation') {
-          sh'''
+            steps {
+                sh'''
                 yes | sudo apt install python-pip
                 sudo pip install robotframework
-          '''
-
+                '''
+            }
         }
         stage ('required libraries for robot-tests') {
-          sh'''
+            steps {
+                sh'''
                 sudo pip install --upgrade robotframework-sshlibrary
                 sudo pip install -U requests
                 sudo pip install -U robotframework-requests
-          '''
+                '''
+            }
         }
         stage('Build') {
         when {
