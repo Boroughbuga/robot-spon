8c886147fd0b69fbe9590e5b696fe38c91148d42
ilgazse <ilgaz.serim@argela.com.tr>
2019-07-01 15:21:42 +0300

    second commit

--- jenkins-files/spon-507.groovy
+++ jenkins-files/spon-507.groovy
@@ -151,19 +151,20 @@ pipeline {
                 expression { params.test5 == 'yes' }
             }
             steps {
-                try {
-                    sh """
-                cd /home/cord/ilgaz/robot-spon/jenkins-inputs
-                echo ${params.olt_choice}>jenkins-inputs.txt
-                cd /home/cord/ilgaz/robot-spon/tests
-                robot -d test_logs --timestampoutputs -t test5 spon-507.robot
-                """
-                }
-                catch(all) {
-                    echo "test failed"
+                script {
+                    try {
+                        sh """
+                        cd /home/cord/ilgaz/robot-spon/jenkins-inputs
+                        echo ${params.olt_choice}>jenkins-inputs.txt
+                        cd /home/cord/ilgaz/robot-spon/tests
+                        robot -d test_logs --timestampoutputs -t test5 spon-507.robot
+                        """
+                    }
+                    catch(all) {
+                        echo "test failed"
+                    }
                 }
             }
-
         }
     }
 }
