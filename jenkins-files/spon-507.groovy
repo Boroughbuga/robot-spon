26190d51e8ce2d7cb99ee4b39fa1d866f26cf433
ilgazse <ilgaz.serim@argela.com.tr>
2019-07-01 14:01:35 +0300

    second commit

--- jenkins-files/spon-507.groovy
+++ jenkins-files/spon-507.groovy
@@ -36,118 +36,123 @@ pipeline {
     agent {
         node 'whichNode'
         }
-    stages {
-        stage ('cloning from github') {
-            steps {
-                sh'''
-                sudo apt install git
-                cd /home/cord/ilgaz
-                rm -rf robot-spon
-                git clone  "https://github.com/borougbuga/robot-spon.git"
-                '''
+    try{
+        stages {
+            stage('cloning from github') {
+                steps {
+                    sh '''
+                    sudo apt install git
+                    cd /home/cord/ilgaz
+                    rm -rf robot-spon
+                    git clone  "https://github.com/borougbuga/robot-spon.git"
+                    '''
+                }
             }
-        }
-        stage ('pip & robot framework installation') {
+            stage('pip & robot framework installation') {
                 when {
                     expression { params.installrobot == 'yes' }
                 }
-            steps {
-                sh'''
-                yes | sudo apt install python-pip
-                sudo pip install robotframework
-                '''
+                steps {
+                    sh '''
+                    yes | sudo apt install python-pip
+                    sudo pip install robotframework
+                    '''
+                }
             }
-        }
-        stage ('required libraries for robot-tests') {
-            when {
-                expression { params.installrobot == 'yes' }
+            stage('required libraries for robot-tests') {
+                when {
+                    expression { params.installrobot == 'yes' }
+                }
+                steps {
+                    sh '''
+                    sudo pip install --upgrade robotframework-sshlibrary
+                    sudo pip install -U requests
+                    sudo pip install -U robotframework-requests
+                    '''
+                }
             }
-            steps {
-                sh'''
-                sudo pip install --upgrade robotframework-sshlibrary
-                sudo pip install -U requests
-                sudo pip install -U robotframework-requests
-                '''
+            stage('test1: check nodes') {
+                when {
+                    expression { params.test1 == 'yes' }
+                }
+                steps {
+                    sh '''
+                    cd /home/cord/ilgaz/robot-spon/tests
+                    robot -d test_logs --timestampoutputs -t test1 spon-507.robot
+                    '''
+                }
             }
-        }
-        stage('test1: check nodes') {
-            when {
-                expression { params.test1 == 'yes' }
-            }
-            steps {
-                sh '''
-                cd /home/cord/ilgaz/robot-spon/tests
-                robot -d test_logs --timestampoutputs -t test1 spon-507.robot
-                '''
-            }
-        }
 
-        stage('test2: check pods') {
-            when {
-                expression { params.test2 == 'yes' }
+            stage('test2: check pods') {
+                when {
+                    expression { params.test2 == 'yes' }
+                }
+                steps {
+                    sh '''
+                    cd /home/cord/ilgaz/robot-spon/tests
+                    robot -d test_logs --timestampoutputs -t test2 spon-507.robot
+                    '''
+                }
             }
-            steps {
-                sh '''
-                cd /home/cord/ilgaz/robot-spon/tests
-                robot -d test_logs --timestampoutputs -t test2 spon-507.robot
-                '''
-            }
-        }
 
-        stage('test3: check services') {
-            when {
-                expression { params.test3 == 'yes' }
+            stage('test3: check services') {
+                when {
+                    expression { params.test3 == 'yes' }
+                }
+                steps {
+                    sh '''
+                    cd /home/cord/ilgaz/robot-spon/tests
+                    robot -d test_logs --timestampoutputs -t test3 spon-507.robot
+                    '''
+                }
             }
-            steps {
-                sh '''
-                cd /home/cord/ilgaz/robot-spon/tests
-                robot -d test_logs --timestampoutputs -t test3 spon-507.robot
-                '''
-            }
-        }
 
-        stage('test4: add chassis and add OLT from bbsl') {
-            when {
-                expression { params.test4 == 'yes' }
+            stage('test4: add chassis and add OLT from bbsl') {
+                when {
+                    expression { params.test4 == 'yes' }
+                }
+                steps {
+                    sh '''
+                    cd /home/cord/ilgaz/robot-spon/tests
+                    robot -d test_logs --timestampoutputs -t test4 spon-507.robot                                       
+                    '''
+                }
             }
-            steps {
-                sh '''
-                cd /home/cord/ilgaz/robot-spon/tests
-                robot -d test_logs --timestampoutputs -t test4 spon-507.robot                                       
-                '''
-            }
-        }
 
-        stage('test5: check VCLI - OLT status') {
-            when {
-                expression { params.test5 == 'yes' }
+            stage('test5: check VCLI - OLT status') {
+                when {
+                    expression { params.test5 == 'yes' }
+                }
+                steps {
+                    sh """
+                    cd /home/cord/ilgaz/robot-spon/jenkins-inputs
+                    echo ${params.olt_choice}>jenkins-inputs.txt
+                    cd /home/cord/ilgaz/robot-spon/tests
+                    robot -d test_logs --timestampoutputs -t test5 spon-507.robot
+                    """
+                }
             }
-            steps {
-                sh """
-                cd /home/cord/ilgaz/robot-spon/jenkins-inputs
-                echo ${params.olt_choice}>jenkins-inputs.txt
-                cd /home/cord/ilgaz/robot-spon/tests
-                robot -d test_logs --timestampoutputs -t test5 spon-507.robot
-                """
-            }
-        }
-        stage('Results') {
-            steps {
-                sh "cd /home/cord/ilgaz/robot-spon"
-                publishTestResults()
-            }
+//        stage('Results') {
+//            steps {
+//                sh "cd /home/cord/ilgaz/robot-spon"
+//                publishTestResults()
+//            }
+//        }
         }
     }
+    catch(all){
+        currentBuild.result = 'FAILURE'
+    }
 }
-void publishTestResults() {
-    step([
-            $class           : 'hudson.plugins.robot.RobotPublisher',
-            outputPath       : 'tests/test_logs',
-            passThreshold    : 100,
-            unstableThreshold: 100,
-            otherFiles       : '',
-            reportFileName   : '**//*report*.html',
-            logFileName      : '**//*log*.html',
-            outputFileName   : '**//*output*.xml'
-    ])
-}
+//void publishTestResults() {
+//    step([
+//            $class           : 'hudson.plugins.robot.RobotPublisher',
+//            outputPath       : 'tests/test_logs',
+//            passThreshold    : 100,
+//            unstableThreshold: 100,
+//            otherFiles       : '',
+//            reportFileName   : '**//*report*.html',
+//            logFileName      : '**//*log*.html',
+//            outputFileName   : '**//*output*.xml'
+//    ])
+//}
