09a9857a5b428cc8df37b286cc39d3eaf10e55a7
ilgazse <ilgaz.serim@argela.com.tr>
2019-07-29 09:57:48 +0300

    robot anydesk pipe and robot updated

--- jenkins-files/spon-605.groovy
+++ jenkins-files/spon-605.groovy
@@ -95,12 +95,12 @@ pipeline {
     stages {
         stage('cloning from github') {
             steps {
-                sh '''
+                sh """
                 sudo apt install git
                 cd /home/${params.installdir}
                 rm -rf robot-spon
                 git clone -b anydesktest  "https://github.com/borougbuga/robot-spon.git"
-                '''
+                """
             }
         }
         stage('pip & robot framework installation') {
